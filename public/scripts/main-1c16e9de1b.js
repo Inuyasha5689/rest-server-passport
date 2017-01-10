d0378371f0160f39eb8bfff379da06139f11474f
Drake Scott <dascott0204@gmail.com>
2017-01-09 20:25:28 -0600

    forgot to add .com to my domain

--- public/scripts/main-1c16e9de1b.js
+++ public/scripts/main-1c16e9de1b.js
@@ -622,7 +622,7 @@ angular.module('confusionApp')
 'use strict';
 
 angular.module('confusionApp')
-    .constant("baseURL", "https://magicalexwuff:80/")
+    .constant("baseURL", "https://magicalexwuff.com:80/")
     .factory('menuFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
             return $resource(baseURL + "dishes/:id", null, {
