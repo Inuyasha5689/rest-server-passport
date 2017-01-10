e61ae904c7efd8888e72766ce0ba22053cd66ede
Drake Scott <dascott0204@gmail.com>
2017-01-09 21:28:07 -0600

    deleted unnecessary code in my facotry js

--- public/scripts/main-1c16e9de1b.js
+++ public/scripts/main-1c16e9de1b.js
@@ -633,27 +633,6 @@ angular.module('confusionApp')
 
     }])
 
-    .directive('modalDialog', function() {
-        return {
-            restrict: 'E',
-            replace: true,
-            transclude: true,
-            link: function(scope) {
-                scope.cancel = function() {
-                    scope.$dismiss('cancel');
-                };
-            },
-            template:
-            "<div>" +
-            "<div class='modal-header'>" +
-            "<h3 ng-bind='dialogTitle'></h3>" +
-            "<div ng-click='cancel()'>X</div>" +
-            "</div>" +
-            "<div class='modal-body' ng-transclude></div>" +
-            "</div>"
-        };
-    })
-
     .factory('corporateFactory', ['$resource', 'baseURL', function ($resource, baseURL) {
 
 
