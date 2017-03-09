df964153e0b03e4a91b866f76dc11131e32a9d4b
Drake Scott <dascott0204@gmail.com>
2017-03-09 16:17:35 -0600

    got the modals working

--- public/scripts/main-8d44041ae4.js
+++ public/scripts/main-8d44041ae4.js
@@ -686,17 +686,22 @@ angular.module('confusionApp')
     
     }])
 
-    .controller('LoginController', ['$scope', '$localStorage', 'AuthFactory', function ($scope, $localStorage, AuthFactory) {
+    .controller('LoginController', ['$scope', 'ngDialog', '$localStorage', 'AuthFactory', function ($scope, ngDialog, $localStorage, AuthFactory) {
 
         $scope.loginData = $localStorage.getObject('userinfo','{}');
 
-        $scope.doLogin = function () {
-            if($scope.rememberMe){
+        $scope.doLogin = function() {
+            if($scope.rememberMe)
                 $localStorage.storeObject('userinfo',$scope.loginData);
-            }
 
             AuthFactory.login($scope.loginData);
-            $scope.closeLogin();
+
+            ngDialog.close();
+
+        };
+
+        $scope.openRegister = function () {
+            ngDialog.open({ template: 'views/register.html', scope: $scope, className: 'ngdialog-theme-default', controller:"RegisterController" });
         };
 
     }])
@@ -711,7 +716,7 @@ angular.module('confusionApp')
 
             AuthFactory.register($scope.registration);
 
-            $scope.closeRegister();
+             ngDialog.close();
         };
     }])
 
